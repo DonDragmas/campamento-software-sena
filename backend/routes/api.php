5765de927db211e1c6b5e53cf44b313c8dac4f10
Leonardoch6 <lchaparro030@misena.edu.co>
2022-08-26 11:54:36 -0500

    rutas rest

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BootcampController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    //return $request->user();
+//});
+
+//La clase route: es una clase laravel encargado de rutas
+//Metodo apiResource: Crea rutas de cambios de estado para una entidad
+Route::apiResource('bootcamps', BootcampController::class);
+
+
+
+
